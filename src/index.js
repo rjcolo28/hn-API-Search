11149eff7bef303498c35704426c4510cab876db
Ryan Colosanti <silentcornball28@Ryans-MacBook-Pro.local>
2019-11-18 16:07:06 -0500

    Reset

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import rootReducer from './reducers/rootReducer';
 
-const store = createStore(rootReducer);
 
-ReactDOM.render(<Provider store={store}> <App /> </Provider>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.register();
